dcdcba1d461ed39117493d74118e0b2ffa331712
conradoruas <conradoruas99@gmail.com>
2023-11-12 21:08:51 -0300

    Testing login

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout: 12000,
+    defaultCommandTimeout: 15000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,6 @@
+describe('Login', () => {
+    it("sucessfully logs in", () => {
+       cy.guiLogin()
+
+    })
+})
