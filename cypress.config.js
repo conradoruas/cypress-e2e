9bfa09c8b5c3b0597bb367c8fd948f6d8ab709fa
conradoruas <conradoruas99@gmail.com>
2023-11-13 00:31:54 -0300

    Testing CRUD

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout: 15000,
+    defaultCommandTimeout: 20000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,3 @@
-// cypress/support/commands.js
-
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
     cy.intercept('GET', '**/notes').as('getNotes')
     cy.visit('/signup')
@@ -37,4 +35,54 @@ Cypress.Commands.add('sessionLogin', (
   ) => {
     const login = () => cy.guiLogin(username, password)
     cy.session(username, login)
+  })
+
+  const attachFileHandler = () => {
+    cy.get('#file').selectFile('cypress/fixtures/example.json')
+  }
+  
+  Cypress.Commands.add('createNote', (note, attachFile = false) => {
+    cy.visit('/notes/new')
+    cy.get('#content').type(note)
+  
+    if (attachFile) {
+      attachFileHandler()
+    }
+  
+    cy.contains('button', 'Create').click()
+  
+    cy.contains('.list-group-item', note).should('be.visible')
+  })
+  
+  Cypress.Commands.add('editNote', (note, newNoteValue, attachFile = false) => {
+    cy.intercept('GET', '**/notes/**').as('getNote')
+  
+    cy.contains('.list-group-item', note).click()
+    cy.wait('@getNote')
+  
+    cy.get('#content')
+      .as('contentField')
+      .clear()
+    cy.get('@contentField')
+      .type(newNoteValue)
+  
+    if (attachFile) {
+      attachFileHandler()
+    }
+  
+    cy.contains('button', 'Save').click()
+  
+    cy.contains('.list-group-item', newNoteValue).should('be.visible')
+    cy.contains('.list-group-item', note).should('not.exist')
+  })
+  
+  Cypress.Commands.add('deleteNote', note => {
+    cy.contains('.list-group-item', note).click()
+    cy.contains('button', 'Delete').click()
+  
+    cy.get('.list-group-item')
+      .its('length')
+      .should('be.at.least', 1)
+    cy.contains('.list-group-item', note)
+      .should('not.exist')
   })
